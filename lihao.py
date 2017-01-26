9885dccfc587371272cee27a0448f977e8bb4fba
kevenli <pbleester@gmail.com>
2017-01-26 10:32:42 +0800

    fast route, and dangerous rendering

--- lihao.py
+++ lihao.py
@@ -5,6 +5,7 @@ import datetime
 logger = logging.getLogger(__name__)
 import copy
 
+
 import sys
 class TailRecurseException:
    def __init__(self, args, kwargs):
@@ -78,7 +79,11 @@ class Room(object):
 
     @property
     def exits(self):
-        return [self.north, self.west, self.south, self.east]
+        return {'north': self.north,
+                'west': self.west,
+                'south': self.south,
+                'east': self.east
+                }
 
     def __str__(self):
         return '(%d,%d)' % (self.x, self.y)
@@ -125,39 +130,84 @@ class Map(object):
         return self.martix[x % self.width][y % self.height]
 
     def print_map(self):
+        import color_console
         for j in range(self.height):
             for i in range(self.width):
                 #point = self.get_point(i,j)
                 point = self.rooms[(i,j)]
+                if point.danger == 0:
+                    color_console.set_text_attr(color_console.FOREGROUND_GREEN)
+                elif point.danger < 1:
+                    color_console.set_text_attr(color_console.FOREGROUND_YELLOW)
+                else:
+                    color_console.set_text_attr(color_console.FOREGROUND_RED)
+
+
                 if point.has_snake:
                     print 'x',
                 elif point.has_bean:
                     print 'o',
                 else:
-                    print ' ',
+                    print '.',
             print
+        color_console.set_text_attr(color_console.FOREGROUND_INTENSITY)
 
     def expend_map(self):
         self.expend_matrix = [[DummyPoint(i,j, self.get_point(i,j)) for j in range(self.height*3)] for i in range(self.width*3)]
 
+
+    def route_directions(self, route):
+        directions = []
+        current_dir = None
+        dirs = ['west', 'north', 'south', 'east']
+        for i, step in enumerate(route[0:-1]):
+            next_step = route[i+1]
+            for dir in dirs:
+                if step.exits[dir] == next_step:
+                    if dir != current_dir:
+                        current_dir = dir
+                        directions.append(current_dir)
+                    break
+
+        return directions
+
+    def distance(self, start_room, end_room):
+        min_distance = min(abs(start_room.x - end_room.x), abs(start_room.x - self.width - end_room.x)) + \
+            min(abs(start_room.y - end_room.y), abs(start_room.y - self.height - end_room.y))
+        return min_distance
+
+
+
     #@tail_call_optimized
     def find_way(self, start_room, end_room, routes, route=None, max_steps=150, depth=0):
-        min_distance = min(abs(start_room.x - end_room.x), abs(start_room.x + self.width - end_room.x)) + \
-            min(abs(start_room.y - end_room.y), abs(start_room.y + self.height - end_room.y))
+        min_distance = self.distance(start_room, end_room)
         if route is None:
             route = []
+
+        if len(self.route_directions(route))>=6:
+            return
+
 
         #if len(route) >= max_steps:
         #    return
         if depth >= max_steps:
             return
 
-        for exit in start_room.exits:
+        for exit in start_room.exits.values():
             if exit in route:
+                # circle
                 continue
 
             if exit.has_snake:
+                # snake blocked
                 continue
+
+            if self.distance(exit, end_room) > min_distance:
+                # shortest way
+               continue
+
+            # if exit.danger >= 1:
+            #     continue
             #logger.debug(exit)
             route_copy = copy.copy(route)
             #logger.debug(route_copy)
@@ -167,6 +217,30 @@ class Map(object):
             else:
                 # check if not walked
                 self.find_way(exit, end_room, routes, route_copy, max_steps=max_steps, depth=depth+1)
+
+    def render_dangerous(self, snake_heads=None, depth=0, max_depth=3):
+        '''
+            compute oppotunity of blocks each snake will go
+
+        :param snake_heads:  snake head blocks, pass None for the first cursive, it will load snakes from map.
+        :param depth: current recursive depth
+        :param max_depth: the round count to forecast
+        :return:
+        '''
+        if depth >= max_depth:
+            return
+
+        if snake_heads is None:
+            snake_heads = [self.rooms[(snake.head.x, snake.head.y)] for snake in self.snakes]
+
+        all_exits = []
+        for snake_head in snake_heads:
+            all_exits += snake_head.exits.values()
+        #all_exits = [snake_head.exits.values() for snake_head in snake_heads]
+        for exit in all_exits:
+            exit.danger += (1.0/4**depth)
+
+        self.render_dangerous(all_exits, depth+1, max_depth=max_depth)
 
 
 
@@ -194,6 +268,31 @@ def hli(width, height, snakes, i, np):
 
 #def nearest_distance(map, p_snake_head, p_bean):
 
+def test_routes(map):
+    start_room = map.rooms[(0, 0)]
+    end_room = map.rooms[(5, 8)]
+
+    map.render_dangerous()
+
+    routes = []
+
+    start = datetime.datetime.now()
+    map.find_way(start_room=start_room, end_room=end_room, routes=routes, max_steps=max_steps)
+    # print list(routes)
+    end = datetime.datetime.now()
+
+    for i in range(len(routes)):
+        route = routes[i]
+        print 'route: %d, distance: %d, %s' % (i, len(route), route)
+
+    print (end - start).total_seconds()
+    print len(routes)
+    print routes[0]
+
+def test_dangerous(map):
+    map.render_dangerous()
+    map.print_map()
+
 
 
 if __name__ == '__main__':
@@ -201,28 +300,12 @@ if __name__ == '__main__':
     #hli(100, 100, [[[0,0],[0,1]], [[2,2], [3,2]]], 1, [0,42])
     #hli(4, 4, [[[0, 0], [0, 1]], [[1, 1], [1, 2]]], 1, [0, 3])
 
-    width = 7
-    height = 7
+    width = 30
+    height = 30
     #max_steps = 15
-    max_steps = width + height + 1
+    max_steps = (width + height) / 2 + 1
     map = Map(width, height, [[[0, 0], [0, 1]], [[1, 1], [1, 2]]], 1, [0, 3])
-    start_room = map.rooms[(0,0)]
-    end_room = map.rooms[(3,2)]
 
+    test_dangerous(map)
 
-    routes = []
-
-    start = datetime.datetime.now()
-    map.find_way(start_room=start_room, end_room=end_room, routes=routes, max_steps=max_steps)
-    #print list(routes)
-    end = datetime.datetime.now()
-
-
-    for i in range(len(routes)):
-        route = routes[i]
-        print 'route: %d, distance: %d' % (i, len(route))
-
-    print (end - start).total_seconds()
-    print len(routes)
-    print routes[0]
 
